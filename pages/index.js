168dbab51f93afa90dede5f412e384a9849aa3a4
warwickdaniel <99989800+warwickdaniel@users.noreply.github.com>
2022-02-18 21:02:19 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>BATH STREET BABES</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
